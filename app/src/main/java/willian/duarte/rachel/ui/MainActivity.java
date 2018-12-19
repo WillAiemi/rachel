9e9eb865814c9cb32d906998ab496b683828b0fb
WillAiemi <willianm.cduarte@gmail.com>
2018-12-19 11:32:43 -0200

    safe build

--- app/src/main/java/willian/duarte/rachel/ui/MainActivity.java
+++ app/src/main/java/willian/duarte/rachel/ui/MainActivity.java
@@ -1,51 +1,47 @@
 package willian.duarte.rachel.ui;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.CalendarView;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.Calendar;
 
 import willian.duarte.rachel.R;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvDate;
-    private Button btCalendar;
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    mTextMessage.setText(R.string.title_home);
+                    return true;
+                case R.id.navigation_dashboard:
+                    mTextMessage.setText(R.string.title_dashboard);
+                    return true;
+                case R.id.navigation_notifications:
+                    mTextMessage.setText(R.string.title_notifications);
+                    return true;
+            }
+            return false;
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        init();
-        if (getIntent().hasExtra("date")){
-            String date = getIntent().getStringExtra("date");
-            tvDate.setText(date);
-        }
-
-
-        btCalendar.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent it = new Intent(MainActivity.this,Main2Activity.class);
-                startActivity(it);
-            }
-        });
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setSelectedItemId(R.id.navigation_home);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
-    private void init(){
-        tvDate = findViewById(R.id.ma_tv_date);
-        btCalendar = findViewById(R.id.ma_bt_calendar);
-    }
-
-    private void toast(String msg){
-        Toast.makeText(getBaseContext(),msg,Toast.LENGTH_SHORT);
-    }
 }
