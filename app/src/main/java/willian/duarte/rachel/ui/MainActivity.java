6fe64d5441a7c581d301db87e1f9edd0e703fc1d
WillAiemi <willianm.cduarte@gmail.com>
2018-12-19 17:00:54 -0200

    Tests

--- app/src/main/java/willian/duarte/rachel/ui/MainActivity.java
+++ app/src/main/java/willian/duarte/rachel/ui/MainActivity.java
@@ -3,24 +3,56 @@ package willian.duarte.rachel.ui;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
 
 import willian.duarte.rachel.R;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FrameLayout flMainFrame;
+    private FloatingActionsMenu fabMenu;
+    private com.getbase.floatingactionbutton.FloatingActionButton fabAddEvent;
+    private com.getbase.floatingactionbutton.FloatingActionButton fabAddClothes;
+    private TextView labelAddEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         init();
+        fabAddEvent.setColorNormal(R.color.colorPrimaryDark);
 
-    }
+        fabMenu.setOnFloatingActionsMenuUpdateListener(new FloatingActionsMenu.OnFloatingActionsMenuUpdateListener() {
+            @Override
+            public void onMenuExpanded() {
+                labelAddEvent.setVisibility(View.VISIBLE);
+                toast(fabAddEvent.getTitle());
+            }
+
+            @Override
+            public void onMenuCollapsed() {
+                labelAddEvent.setVisibility(View.INVISIBLE);
+            }
+        });
+
+    } //close onCreate();
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.ma_fab_add_event:
+                toast("hell yeah!");
+                return;
+        }
+    } // close onClick();
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -37,7 +69,7 @@ public class MainActivity extends AppCompatActivity {
             }
             return false;
         }
-    };
+    }; // close navigation onClickListeners
 
     private void init(){
         flMainFrame = findViewById(R.id.ma_fl_mainframe);
@@ -46,6 +78,19 @@ public class MainActivity extends AppCompatActivity {
         navigation.setSelectedItemId(R.id.navigation_home);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-    }
+        fabMenu = findViewById(R.id.ma_fabm_menu);
 
+        fabAddEvent = findViewById(R.id.ma_fab_add_event);
+        fabAddEvent.setOnClickListener(this);
+        fabAddEvent.setTitle("arroba");
+
+        fabAddClothes = findViewById(R.id.ma_fab_add_clothes);
+
+        labelAddEvent = findViewById(R.id.ma_tv_addevent);
+        labelAddEvent.setVisibility(View.INVISIBLE);
+    }// close init();
+
+    private void toast(String msg){
+        Toast.makeText(getBaseContext(),msg,Toast.LENGTH_SHORT).show();
+    }//close toast();
 }
