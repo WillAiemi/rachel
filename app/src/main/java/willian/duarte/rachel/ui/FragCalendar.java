c167749d89ebf59248717105a60d3052d6d49d3d
Willian Duarte <willianm.cduarte@gmail.com>
2018-12-20 01:36:17 -0200

    Added fragments

---
+++ app/src/main/java/willian/duarte/rachel/ui/FragCalendar.java
@@ -0,0 +1,30 @@
+package willian.duarte.rachel.ui;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import willian.duarte.rachel.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class FragCalendar extends Fragment {
+
+
+    public FragCalendar() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_frag_calendar, container, false);
+    }
+
+}
--- app/src/main/java/willian/duarte/rachel/ui/MainActivity.java
+++ app/src/main/java/willian/duarte/rachel/ui/MainActivity.java
@@ -1,9 +1,10 @@
 package willian.duarte.rachel.ui;
 
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
-import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -21,7 +22,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private FloatingActionsMenu fabMenu;
     private com.getbase.floatingactionbutton.FloatingActionButton fabAddEvent;
     private com.getbase.floatingactionbutton.FloatingActionButton fabAddClothes;
-    private TextView labelAddEvent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,17 +29,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         init();
         fabAddEvent.setColorNormal(R.color.colorPrimaryDark);
-
         fabMenu.setOnFloatingActionsMenuUpdateListener(new FloatingActionsMenu.OnFloatingActionsMenuUpdateListener() {
             @Override
             public void onMenuExpanded() {
-                labelAddEvent.setVisibility(View.VISIBLE);
                 toast(fabAddEvent.getTitle());
             }
 
             @Override
             public void onMenuCollapsed() {
-                labelAddEvent.setVisibility(View.INVISIBLE);
+
             }
         });
 
@@ -60,11 +58,26 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
+                case R.id.nav_menu_item_home:
+                    android.support.v4.app.FragmentManager fgm = getSupportFragmentManager();
+                    android.support.v4.app.FragmentTransaction ft = fgm.beginTransaction();
+                    FragCalendar fragCalendar = new FragCalendar();
+                    ft.replace(R.id.ma_fl_mainframe,fragCalendar);
+                    ft.commit();
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.nav_menu_item_wardrobe:
+                    android.support.v4.app.FragmentManager fgm2 = getSupportFragmentManager();
+                    android.support.v4.app.FragmentTransaction ft2 = fgm2.beginTransaction();
+                    FragWardrobe fragWardrobe = new FragWardrobe();
+                    ft2.replace(R.id.ma_fl_mainframe,fragWardrobe);
+                    ft2.commit();
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.nav_menu_item_settings:
+                    android.support.v4.app.FragmentManager fgm3 = getSupportFragmentManager();
+                    android.support.v4.app.FragmentTransaction ft3 = fgm3.beginTransaction();
+                    FragWardrobe fragWardrobe2 = new FragWardrobe();
+                    ft3.replace(R.id.ma_fl_mainframe,fragWardrobe2);
+                    ft3.commit();
                     return true;
             }
             return false;
@@ -74,9 +87,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void init(){
         flMainFrame = findViewById(R.id.ma_fl_mainframe);
 
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setSelectedItemId(R.id.navigation_home);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.ma_nav);
+        navigation.setSelectedItemId(R.id.nav_menu_item_home);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        FragmentManager fgm = getSupportFragmentManager();
+        android.support.v4.app.FragmentTransaction ft = fgm.beginTransaction();
+        FragCalendar fragCalendar = new FragCalendar();
+        ft.add(R.id.ma_fl_mainframe,fragCalendar);
+        ft.commit();
 
         fabMenu = findViewById(R.id.ma_fabm_menu);
 
@@ -85,9 +103,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         fabAddEvent.setTitle("arroba");
 
         fabAddClothes = findViewById(R.id.ma_fab_add_clothes);
-
-        labelAddEvent = findViewById(R.id.ma_tv_addevent);
-        labelAddEvent.setVisibility(View.INVISIBLE);
     }// close init();
 
     private void toast(String msg){
